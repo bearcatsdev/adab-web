441967fdb8befe2d21957474eb0fed2830a0f4fd
Stanley <angstanley68@gmail.com>
2020-07-09 19:56:40 +0700

    Initialize TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: false,
+  theme: {
+    extend: {
+      colors: {
+        primary: '#007BFF',
+        primaryDark: '#0067d4',
+        info: '#30A8E6',
+        success: '#A1C920',
+        danger: '#FC5842',
+        dark: '#343A40',
+        secondary: '#6C757D',
+        black: '#000',
+        white: '#fff',
+        body: '#3C4859',
+        transparent: 'transparent'
+      },
+      container: {
+        padding: {
+          default: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+        },
+      },
+      fontFamily: {
+        'body': ['Inter']
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      },
+      boxShadow: {
+        'primary': '0 10px 15px -3px rgba(48, 168, 230, .3)'
+      },
+      screens: {
+        'dark-mode': { raw: '(prefers-color-scheme: dark)' }
+      }
+    },
+  },
+  variants: {},
+  plugins: [
+    require('autoprefixer'),
+    require('tailwindcss'),
+  ],
+}
