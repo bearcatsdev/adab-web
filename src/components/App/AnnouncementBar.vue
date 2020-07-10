20f8dbda93b454220218af129dfabf917ca7c8fe
Stanley <angstanley68@gmail.com>
2020-07-10 12:16:43 +0700

    Create announcement bar

---
+++ src/components/App/AnnouncementBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="announcement-bar">
+        <div class="content">
+            <div class="title">Announcement</div>
+            <div class="body">Pengumuman Auto Debet Biaya Kuliah Semester Genap 2018/2019</div>
+        </div>
+        <div @click="closeAnnouncement"><i class="close-icon" data-feather="x"/></div>
+    </div>
+</template>
+
+<script>
+    import feather from "feather-icons";
+
+    export default {
+        name: "AnnouncementBar",
+        methods: {
+            closeAnnouncement() {
+                this.$emit('close')
+            }
+        },
+        mounted() {
+            feather.replace()
+        },
+    }
+</script>
+
+<style scoped>
+    .announcement-bar {
+        @apply w-full bg-primary text-white flex justify-between py-3 px-4;
+    }
+
+    .content {
+        @apply flex;
+    }
+
+    .body {
+        @apply ml-6;
+    }
+
+    .title {
+        @apply font-medium;
+    }
+
+    .close-icon:hover {
+        @apply cursor-pointer;
+    }
+</style>
--- src/views/App/Dashboard.vue
+++ src/views/App/Dashboard.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-        hellowwww
+        <announcement-bar v-if="showAnnouncement" @close="showAnnouncement = false"/>
     </div>
 </template>
 
 <script>
+    import AnnouncementBar from "../../components/App/AnnouncementBar";
     export default {
-        name: "Dashboard"
+        name: "Dashboard",
+        components: {AnnouncementBar},
+        data () {
+            return {
+                showAnnouncement: true
+            }
+        },
+        methods: {
+        }
     }
 </script>
 
