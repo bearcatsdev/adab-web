7840b08f808c070fb5b8c57da1241a2c0ccdd377
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-07-12 17:23:51 +0700

    Clean up some code
    1. Remove useless components and views
    2. Clean up vue router's index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
+    redirect: '/login'
   },
   {
     path: '/login',
