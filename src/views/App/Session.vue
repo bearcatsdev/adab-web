6dcf9da6bb4f8fe263bdc26c995b93f70377fd70
Stanley <angstanley68@gmail.com>
2020-07-13 08:15:58 +0700

    Implement socket.io to session page
    #14

--- src/views/App/Session.vue
+++ src/views/App/Session.vue
@@ -4,23 +4,92 @@
                  icon="book-open"
                  :title="`${sessionDetails['course_name']}: ${sessionDetails['topic_title']}`"
                  :subtitle="`${sessionDetails['course_id']} - ${sessionDetails['lecturer_name']} (${sessionDetails['course_lang']})`">
-            {{ content }}
+            <div v-if="currentUser['can_talk']">
+                <Button @click="toggleTalk">{{ talk ? 'Stop' : 'Start' }} talking</Button>
+            </div>
+            <div>
+                {{ content }}
+                <span class="blinking-cursor" v-if="blinkingCursor">|</span>
+            </div>
         </Section>
     </div>
 </template>
 
 <script>
-    import Section from "../../components/App/Section";
-    import SessionApi from "../../services/api/Session";
+    import Section from "../../components/App/Section"
+    import SessionApi from "../../services/api/Session"
+    import io from "socket.io-client"
+    import {mapState} from "vuex";
+    import Button from "../../components/Button";
+
+    const socket = io.connect('https://adabapi.bearcats.dev')
+    let recognition
 
     export default {
         name: "Session",
-        components: {Section},
-        data () {
+        components: {Button, Section},
+        data() {
             return {
                 sessionId: this.$route.params.id,
                 sessionDetails: {},
-                content: ''
+                content: '',
+                talk: false,
+                blinkingCursor: false
+            }
+        },
+        computed: {
+            ...mapState('UserCredentials', ['currentUser'])
+        },
+        methods: {
+            toggleTalk() {
+                this.talk = !this.talk
+                if (this.talk) {
+                    this.startSpeechRecognition()
+                } else {
+                    this.stopSpeechRecognition()
+                }
+            },
+            startSpeechRecognition() {
+                // speech recognition
+                this.talk = true
+                socket.emit('start_talking')
+                recognition = new (window.speechRecognition || window.webkitSpeechRecognition)()
+                recognition.interimResults = false
+                recognition.lang = this.sessionDetails['course_lang']
+                recognition.start()
+
+                recognition.onresult = (event) => {
+                    const recognized = event.results[0][0].transcript
+                    // console.log(recognized)
+                    socket.emit('message', recognized)
+                }
+
+                recognition.onend = () => {
+                    recognition.stop()
+                    recognition.start()
+                };
+            },
+            stopSpeechRecognition() {
+                this.talk = false
+                socket.emit('stop_talking')
+                recognition.stop()
+                recognition = null
+            },
+            setupSocketIo() {
+                // socket.io
+                socket.on('message', (message) => {
+                    this.content += ` ${message}`
+                });
+
+                socket.on('start_talking', () => {
+                    this.blinkingCursor = true
+                });
+
+                socket.on('stop_talking', () => {
+                    this.blinkingCursor = false
+                });
+
+                socket.emit('join_room', this.sessionId);
             }
         },
         mounted() {
@@ -28,31 +97,16 @@
                 this.sessionDetails = response.data.values
                 this.content = this.sessionDetails['content']
 
-                // setup speech recognition
-                setupSpeechRecognition(this.sessionDetails['course_lang'])
+                // setup socket.io
+                this.setupSocketIo()
             }).catch((error) => {
                 // do something when error
                 console.log(error)
             })
-
-            const setupSpeechRecognition = (lang) => {
-                // speech recognition
-                const recognition = new (window.speechRecognition || window.webkitSpeechRecognition)()
-                recognition.interimResults = false;
-                recognition.lang = lang
-                recognition.start()
-
-                recognition.onresult = (event) => {
-                    const recognized = event.results[0][0].transcript
-                    // console.log(recognized)
-                    this.content += ` ${recognized}`
-                }
-
-                recognition.onend = () => {
-                    recognition.stop();
-                    recognition.start();
-                };
-            }
+        },
+        beforeDestroy() {
+            // stop listening
+            if (this.talk) this.stopSpeechRecognition()
         }
     }
 </script>
@@ -64,5 +118,62 @@
 
     .section {
         @apply w-full;
+    }
+
+    .blinking-cursor {
+        -webkit-animation: 1s blink step-end infinite;
+        -moz-animation: 1s blink step-end infinite;
+        -ms-animation: 1s blink step-end infinite;
+        -o-animation: 1s blink step-end infinite;
+        animation: 1s blink step-end infinite;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    @keyframes blink {
+        from, to {
+            color: transparent;
+        }
+        50% {
+            color: black;
+        }
+    }
+
+    @-moz-keyframes blink {
+        from, to {
+            color: transparent;
+        }
+        50% {
+            color: black;
+        }
+    }
+
+    @-webkit-keyframes blink {
+        from, to {
+            color: transparent;
+        }
+        50% {
+            color: black;
+        }
+    }
+
+    @-ms-keyframes blink {
+        from, to {
+            color: transparent;
+        }
+        50% {
+            color: black;
+        }
+    }
+
+    @-o-keyframes blink {
+        from, to {
+            color: transparent;
+        }
+        50% {
+            color: black;
+        }
     }
 </style>
