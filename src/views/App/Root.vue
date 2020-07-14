f8be217feae61646dd61b319abfba47ac7f5991c
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-07-14 17:35:14 +0700

    fix: move api calls from dashboard to root view
    Move api calls to check session and retrive profile data from dashboard to root view.
    
    Fixes #15

--- src/views/App/Root.vue
+++ src/views/App/Root.vue
@@ -8,11 +8,36 @@
 <script>
     import TheNav from "../../components/App/TheNav";
     import {mapState} from "vuex";
+    import feather from 'feather-icons';
+
     export default {
         name: "Home",
         components: {TheNav},
         computed: {
             ...mapState('UserCredentials', ['currentUser'])
+        },
+        mounted() {
+            // get user profile
+            this.$store.dispatch('UserCredentials/GET_CURRENT_USER')
+                .catch(error => {
+                    if (error.response.status === 401) {
+                        // unauthorized
+                        this.$router.push('/login')
+                    }
+                })
+
+            // get user sessions
+            this.$store.dispatch('UserCredentials/GET_SESSIONS')
+                .catch(error => {
+                    if (error.response.status === 401) {
+                        // unauthorized
+                        this.$router.push('/login')
+                    }
+                }).then(() => {
+
+                // feather
+                feather.replace()
+            })
         },
     }
 </script>
