0b87693c6d0046da3dcae8ee7c3824dbbbcaf6d4
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-12-02 00:42:45 +0700

    refactor: change code style

--- src/main.js
+++ src/main.js
@@ -7,15 +7,12 @@ import './assets/css/main.css'
 Vue.config.productionTip = false
 
 
-const DEFAULT_TITLE = 'Adab - Ayo Dengar Ayo Bicara';
-// eslint-disable-next-line no-unused-vars
-router.afterEach((to, from) => {
-  // Use next tick to handle router history correctly
-  // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+const DEFAULT_TITLE = 'Adab - Ayo Dengar Ayo Bicara'
+router.afterEach(to => {
   Vue.nextTick(() => {
-    document.title = to.meta.title || DEFAULT_TITLE;
-  });
-});
+    document.title = to.meta.title || DEFAULT_TITLE
+  })
+})
 
 new Vue({
   router,
